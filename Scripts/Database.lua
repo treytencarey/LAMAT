31e2b17f0276900c2a62c32713bd7c18db2ce99a
treytencarey <treytencarey@gmail.com>
2018-08-03 04:46:40 +0000

    Fixed a problem with SQL literals.

--- Scripts/Database.lua
+++ Scripts/Database.lua
@@ -13,6 +13,10 @@ function Database:UpdateProblem(Title,Description)
   loggedIn = script:triggerFunction("getStatus", "Scripts/login.lua")
   if loggedIn == nil or loggedIn == false then return; end
   userName = script:triggerFunction("getUserName", "Scripts/login.lua")
+
+  Title = formatLiterals(Title)
+  Description = formatLiterals(Description)
+
   server:getSQL(self.dbFile,
     "update Problem SET Description= '" .. Description .. "' WHERE ACCOUNT = '" .. userName .. "' AND Title = '" .. Title .. "';"
   )
@@ -21,6 +25,12 @@ function Database:InsertProblem(Title, Category, Description, Latitude, Longitud
   loggedIn = script:triggerFunction("getStatus", "Scripts/login.lua")
   if loggedIn == nil or loggedIn == false then return; end
   userName = script:triggerFunction("getUserName", "Scripts/login.lua")
+
+  Title = formatLiterals(Title)
+  Category = formatLiterals(Category)
+  Description = formatLiterals(Description)
+  Latitude = formatLiterals(Latitude)
+  Longitude = formatLiterals(Longitude)
 
   server:getSQL(self.dbFile,
     "insert into Problem ( Account, Title, Category, Description, Latitude, Longitude) values ('" .. userName .. "', '" .. Title .. "', '" .. Category .. "','" .. Description .. "', '" .. Latitude  .. "','" .. Longitude .. "');"
@@ -35,13 +45,17 @@ function Database:GetAllProblems()
 end
 
 function Database:GetProblemsByCategory(cat)
+  cat = formatLiterals(cat)
+
   server:getSQL(self.dbFile,
-    "select Title from Problem where Category='" .. cat .. "';",
+    "select Title from Problem where Category=" .. cat .. ";",
     "CategoricalProb"
   )
 end
 
 function Database:ViewProblem(Title, page)
+  Title = formatLiterals(Title)
+
   server:getSQL(self.dbFile,
     "select * from Problem where Title='" .. Title .. "';",
     "viewProblem" .. tostring(page)
@@ -49,6 +63,8 @@ function Database:ViewProblem(Title, page)
 end
 
 function Database:ViewMyProblems(UserId)
+  UserId = formatLiterals(UserId)
+
   server:getSQL(self.dbFile,
     "select Title from Problem where Account='" .. UserId .. "';",
     "allProblems" 
@@ -56,12 +72,17 @@ function Database:ViewMyProblems(UserId)
 end
 
 function Database:createAccount(userName, pass)
+  userName = formatLiterals(userName)
+  pass = formatLiterals(pass)
+
   server:getSQL(self.dbFile,
     "insert into Account (Account, Password) values ('" .. userName .. "', '" .. pass .. "');"
   )
 end
 
 function Database:getUserAccount(userName, reason)
+  userName = formatLiterals(userName)
+
   server:getSQL(self.dbFile,
   "SELECT Account, Password FROM Account WHERE Account='" .. userName .. "';",
   reason
@@ -103,4 +124,9 @@ function onCreated()
   db = Database.new("database/database.db")
   db:CreateProblemsTable()
   db:GetAllProblems()
+end
+
+function formatLiterals(str)
+  str = str:gsub("'", "''")
+  return str
 end
